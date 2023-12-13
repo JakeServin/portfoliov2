29cb63097b0d38a5a0e056fa42079aea3c07a795
Jake Servin <100403428+JakeServin@users.noreply.github.com>
2023-12-13 15:57:03 -0700

    created layout

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,13 +4,13 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <nav className='border-2 border-green-500 flexCenter md:flexStart relative z-30 py-4 bg-zinc-700'>
+    <nav className='flexAround md:flexStart relative z-30 py-4 bg-zinc-700'>
         <ul className='flex padding-container gap-12 h-full lg:pl-12'>
               {NAV_LINKS.map((link) => (
                   <Link
                       href={link.href}
                       key={link.key}
-                      className='regular-16 text-gray-200 cursor-pointer flexCenter transition:all hover:font-bold'
+                      className='regular-16 text-gray-300 cursor-pointer flexCenter transition:all hover:font-bold hover:text-gray-200'
                   >
                       {link.label}
                   </Link>

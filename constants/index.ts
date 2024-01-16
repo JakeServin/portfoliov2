494a374390bc10d631b0908a34ec2e3b95fd16ca
Jake Servin <100403428+JakeServin@users.noreply.github.com>
2024-01-16 13:20:35 -0700

    Add new files and update configurations

--- constants/index.ts
+++
@@ -1,7 +0,0 @@
-// NAVIGATION
-export const NAV_LINKS = [
-	{ href: "/", key: "home", label: "About" },
-	{ href: "/", key: "projects", label: "Projects" },
-	{ href: "/", key: "contact_me", label: "Contact Me" },
-	{ href: "/", key: "resume", label: "Resume" },
-];
